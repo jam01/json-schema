86ff6f1a343a80181da1265649acde5d110938a8
jam01 <jam01@protonmail.com>
2024-05-22 15:47:07 -0500

    filter notsupported tests

--- shared/src/test/scala/io/github/jam01/json_schema/TestSuiteTest.scala
+++ shared/src/test/scala/io/github/jam01/json_schema/TestSuiteTest.scala
@@ -4,9 +4,10 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.{Arguments, MethodSource}
 
-import java.nio.file.{Files, Paths}
+import java.nio.file.{Files, Path, Paths}
 import java.util.*
 import scala.collection.mutable
+import scala.util.Using
 
 class TestSuiteTest {
 
@@ -19,86 +20,61 @@ class TestSuiteTest {
 }
 
 object TestSuiteTest {
-  val registry: mutable.Map[Uri, Schema] = {
+  val NotSupported: Seq[String] = Seq("content.json",
+    "default.json",
+    "id.json",
+    "infinite-loop-detection.json",
+    "patternProperties.json",
+    "refRemote.json",
+    "unevaluatedItems.json",
+    "unevaluatedProperties.json",
+    "vocabulary.json")
+
+  val Registry: mutable.Map[Uri, Schema] = {
     val res = mutable.Map[Uri, Schema]()
 
     // load remotes
-    val remotes = Files.walk(Paths.get(getClass.getClassLoader.getResource("test-suite/remotes/draft2020-12/").toURI))
-    try {
+    Using(Files.walk(resource("test-suite/remotes/draft2020-12/"), 1)) { remotes =>
       remotes.filter(Files.isRegularFile(_))
         .forEach(p => {
           //println(p.toString)
           ujson.read(ujson.Readable.fromPath(p)).transform(SchemaR("file:" + p.toString, reg = res))
         })
-    } finally if (remotes != null) remotes.close()
+    }
 
-    // load meta-schema
-    val meta = Files.walk(Paths.get(getClass.getClassLoader.getResource("meta/").toURI))
-    try {
+    // load meta-schemas
+    Using(Files.walk(resource("meta/"), 1)) { meta =>
       meta.filter(Files.isRegularFile(_))
         .forEach(p => {
           //println(p.toString)
           ujson.read(ujson.Readable.fromPath(p)).transform(SchemaR("file:" + p.toString, reg = res))
         })
-    } finally if (meta != null) meta.close()
+    }
 
     res
   }
 
   def args_provider: List[Arguments] = {
     val args = new ArrayList[Arguments]()
-    args.addAll(args_provider("test-suite/tests/draft2020-12/additionalProperties.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/allOf.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/anchor.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/anyOf.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/boolean_schema.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/const.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/contains.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/defs.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/dependentRequired.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/dependentSchemas.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/dynamicRef.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/enum.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/exclusiveMaximum.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/exclusiveMinimum.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/format.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/if-then-else.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/items.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/maxContains.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/maxItems.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/maxLength.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/maxProperties.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/maximum.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/minContains.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/minItems.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/minLength.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/minProperties.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/minimum.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/multipleOf.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/not.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/oneOf.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/pattern.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/prefixItems.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/properties.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/propertyNames.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/ref.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/required.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/type.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/uniqueItems.json"))
-    args.addAll(args_provider("test-suite/tests/draft2020-12/unknownKeyword.json"))
+    Using(Files.walk(resource("test-suite/tests/draft2020-12/"), 1)) { tests =>
+        tests.filter(Files.isRegularFile(_))
+          .filter(p => !NotSupported.contains(p.getFileName.toString))
+          //.peek(println
+          .forEach(p => args.addAll(args_provider(p)))
+    }
 
     args
   }
 
-  def args_provider(path: String): List[Arguments] = {
-    val suite = ujson.read(ujson.Readable.fromPath(Paths.get(getClass.getClassLoader.getResource(path).toURI))).arr
+  def args_provider(path: Path): List[Arguments] = {
+    val suite = ujson.read(ujson.Readable.fromPath(path)).arr
     val args = new ArrayList[Arguments]()
 
     suite.foreach { testcase =>
       testcase.obj.get("tests").get.arr.foreach { test =>
-        val reg = registry
+        val reg = Registry
         args.add(Arguments.of(
-          path,
+          resource("test-suite/tests/draft2020-12/").relativize(path).toString,
           testcase.obj.get("description").get.str,
           test.obj.get("description").get.str,
           test.obj.get("data").get,
@@ -110,4 +86,6 @@ object TestSuiteTest {
 
     args
   }
+
+  def resource(s: String): Path = Paths.get(getClass.getClassLoader.getResource(s).toURI)
 }
